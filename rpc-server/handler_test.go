d62f46a57d70962e30d950d56afcebab95a148f2
Thang Pham <bathang02@gmail.com>
2023-05-22 16:21:39 +0800

    fix rpc-server test

--- rpc-server/handler_test.go
+++ rpc-server/handler_test.go
@@ -23,7 +23,13 @@ func TestIMServiceImpl_Send(t *testing.T) {
 			name: "success",
 			args: args{
 				ctx: context.Background(),
-				req: &rpc.SendRequest{},
+				req: &rpc.SendRequest{
+					Message: &rpc.Message{
+						Chat:   "test1:test2",
+						Text:   "test",
+						Sender: "test1",
+					},
+				},
 			},
 			wantErr: nil,
 		},
